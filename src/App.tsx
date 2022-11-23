3482dc07ef54502981b5c2dfc85b5bf3b0653cdb
Omar Baldi <omarbaldidk@gmail.com>
2022-11-23 21:42:30 +0100

    Implemented use of react hook useMemo to return a memoized array of cards data containing the image source and the set id

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { v4 as v4Id } from 'uuid';
 import './App.css';
 import { shuffleArr } from './utils/shuffle-arr';
@@ -25,7 +25,6 @@ type ApiResponseType = {
 function App() {
   //TODO: move magic number to descriptive constant variable
   const totalAmountCards = useRef<number>(16);
-
   const [images, setImages] = useState<string[]>([]);
 
   useEffect(() => {
@@ -62,48 +61,39 @@ function App() {
     };
   }, []);
 
+  const memoryCardsData = useMemo(() => {
+    const updatedData = images.reduce<{ imageSrc: string; setId: string }[]>(
+      (acc, imageSrc: string) => {
+        const updatedObj = {
+          imageSrc,
+          setId: v4Id(),
+        };
+
+        return [...acc, updatedObj];
+      },
+      []
+    );
+
+    return shuffleArr([...updatedData, ...updatedData]);
+  }, [images]);
+
   /* if the current card is amongst the one clicked then
   set that to "selected" */
 
   //Map<cardId, setId>
 
-  const memoryGridRef = useCallback((el: HTMLDivElement) => {
-    if (el) {
-      //TODO: check if the totalAmount of cards is an even number
-
-      //Considering the fact that there has to be a pair of 2
-      //the images will have to be half the total amount of cards
-      const amountCardsSet: number = totalAmountCards.current / 2;
-
-      const setIds: string[] = [...Array(amountCardsSet)].map(() => v4Id());
-      const totalAmountSetIds = [...setIds, ...setIds];
-
-      const memoryCardsElement = totalAmountSetIds.reduce<HTMLDivElement[]>(
-        (acc, setId: string, index: number) => {
-          const cardLabel: string = (index + 1).toString();
-
-          const cardElement: HTMLDivElement = document.createElement('div');
-          cardElement.className = 'memory__card';
-          cardElement.innerText = cardLabel;
-          cardElement.dataset.setid = setId;
-
-          acc.push(cardElement);
-
-          return acc;
-        },
-        []
-      );
-
-      for (const card of shuffleArr(memoryCardsElement)) {
-        el.appendChild(card);
-      }
-    }
-  }, []);
-
   return (
     <div className='App'>
       <div className='wrapper'>
-        <div className='memory__grid' ref={memoryGridRef} />
+        <div className='memory__grid'>
+          {memoryCardsData.map(({ imageSrc, setId }, index: number) => {
+            return (
+              <div key={`card-${index}-${setId}`} className='memory__card'>
+                <img src={imageSrc} alt='' className='memory__card__img' />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
